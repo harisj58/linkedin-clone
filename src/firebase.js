cfe89a33d2370e66f5468cf7472843a2a267e0da
Haris Javed <72334266+harisj58@users.noreply.github.com>
2021-12-11 17:10:56 +0530

    Updated firebase.js
    Removed any sensitive information that could have been used maliciously

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACbd55OmjPaN5f6ZEed5qW793dzklEgws",
-  authDomain: "linkedin-clone-4fbba.firebaseapp.com",
-  projectId: "linkedin-clone-4fbba",
-  storageBucket: "linkedin-clone-4fbba.appspot.com",
-  messagingSenderId: "75253081883",
-  appId: "1:75253081883:web:f616f094b1d3014dcc55be",
+  //Update these values based on the firebase.js file you obtain from Firebase
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
